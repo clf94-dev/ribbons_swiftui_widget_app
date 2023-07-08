feece526deb5915c5cb0211c06186c3aad1d179e
clf94-dev <clf94@hotmail.es>
2023-07-08 07:30:43 +0200

    Initial Commit

---
+++ RibbonsWidgetApp/RibbonsWidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RibbonsWidgetAppApp.swift
+//  RibbonsWidgetApp
+//
+//  Created by Carmen Lucas on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RibbonsWidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
