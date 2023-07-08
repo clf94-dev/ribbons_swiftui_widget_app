a1165b80667c20c1ea19b9c396ab8a1deb6886fc
clf94-dev <clf94@hotmail.es>
2023-07-08 08:51:14 +0200

    - scale items according to height of the device

--- RibbonsWidgetApp/Views/AllRibbonsView.swift
+++ RibbonsWidgetApp/Views/AllRibbonsView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 struct AllRibbonsView: View {
     @EnvironmentObject var store: RibbonsDataStore
     var body: some View {
-        let columns = [GridItem](repeating: GridItem(.fixed(170)), count: 2)
+        let columns = [GridItem](repeating: GridItem(.fixed(170 * WidgetSize.scaleFactor)), count: 2)
         NavigationView {
             ScrollView {
                 LazyVGrid (columns: columns, spacing: 10) {
                     ForEach(store.ribbons) {ribbon in
                         RibbonItemView(ribbon: ribbon)
                             .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .scaleEffect(WidgetSize.scaleFactor)
                     }
                     
                 }
@@ -28,7 +29,12 @@ struct AllRibbonsView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        AllRibbonsView()
-            .environmentObject(RibbonsDataStore())
+        Group{
+            AllRibbonsView()
+                .environmentObject(RibbonsDataStore())
+            AllRibbonsView()
+                .preferredColorScheme(.dark)
+                .environmentObject(RibbonsDataStore())
+        }
     }
 }
